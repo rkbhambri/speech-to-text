caffa04b98c8c8f5a28bfea16a0d9430a79c96dc
ram <ramkrishan@kontikilabs.com>
2019-05-17 15:22:32 +0530

    Check browser compatibility

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import BrowserCompatibility from './components/BrowserCompatibility/BrowserCompatibility';
 import Layout from './components/Layout/Layout';
 import './App.css';
 
 function App() {
+	let isBrowserNotCompatible = false;
+
+	if (!window.hasOwnProperty('webkitSpeechRecognition')) {
+		isBrowserNotCompatible = true;
+	}
+
 	return (
 		<div className="app">
-			<Layout />
+			{isBrowserNotCompatible ? <BrowserCompatibility /> : <Layout />}
 		</div>
 	);
 }
